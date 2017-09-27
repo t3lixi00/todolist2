7e22189e31cc00d07b3791c60455a9031ba8d3d3
Rita <t3lixi00@students.oamk.fi>
2017-09-27 15:42:10 +0300

    investigation

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 1337;
 
-//mssql = require('mssql'),
-//db = require('./api/models/todoListModel'),
-//bodyParser = require('body-parser');
+mssql = require('mssql'),
+db = require('./api/models/todoListModel'),
+bodyParser = require('body-parser');
 
 
 
 
-//app.use(bodyParser.urlencoded({ extended: true}));
-//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 //var routes = require('./api/routes/todoListRoutes'); //importing route
 //routes(app); //register the route
