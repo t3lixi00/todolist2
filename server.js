35a96d9c44cafaf73883cdc73b9f2b4cfbc049ae
Rita <t3lixi00@students.oamk.fi>
2017-09-27 15:38:45 +0300

    newchange

--- server.js
+++ server.js
@@ -4,22 +4,24 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 1337;
 
-mssql = require('mssql'),
-db = require('./api/models/todoListModel'),
-bodyParser = require('body-parser');
+//mssql = require('mssql'),
+//db = require('./api/models/todoListModel'),
+//bodyParser = require('body-parser');
 
 
 
 
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true}));
+//app.use(bodyParser.json());
 
-var routes = require('./api/routes/todoListRoutes'); //importing route
-routes(app); //register the route
+//var routes = require('./api/routes/todoListRoutes'); //importing route
+//routes(app); //register the route
 
 
-app.listen(port);
-console.log('to do list RESTful API server started on: ' + port);
+app.listen(port, function() {
+    console.log('to do list RESTful API server started on: ' + port);
+});
+
 
 /*const express = require('express')
 const app = express()
