9eb4b7fa1059a3baefdf56d84e6488a2c491057e
Rita <t3lixi00@students.oamk.fi>
2017-09-27 16:11:44 +0300

    change server14

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ bodyParser = require('body-parser');
 
 
 
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true}));
+//app.use(bodyParser.json());
 
 var routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
