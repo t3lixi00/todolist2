bf36fe2ea110e8b599bb13f5ef31677bd74b1f6b
Rita <t3lixi00@students.oamk.fi>
2017-09-27 15:59:02 +0300

    change server7

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 1337;
 
-mssql = require('mssql'),
-db = require('./api/models/todoListModel'),
+//mssql = require('mssql'),
+//db = require('./api/models/todoListModel'),
 //bodyParser = require('body-parser');
 
 
